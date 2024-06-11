fe23013d4dd7f0fac58c362d86b443c26b067b83
caomengxuan666 <2507560089@qq.com>
2024-06-11 22:12:37 +0800

    更新项目框架结构，防止出现屎山

---
+++ loadUserDataBase.py
@@ -0,0 +1,49 @@
+import hashlib
+import pymysql
+from dbutils.pooled_db import PooledDB
+import uuid
+
+# 配置数据库连接池
+POOL = PooledDB(
+    creator=pymysql,
+    maxconnections=10,
+    mincached=2,
+    maxcached=3,
+    blocking=True,
+    setsession=[],
+    ping=0,
+    host='127.0.0.1',
+    port=3306,
+    user='root',
+    password='mx123321',
+    charset='utf8',
+    db='flask'
+)
+
+def get_db_connection():
+    return POOL.connection()
+
+def register_user(username, password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    user_id = str(uuid.uuid4())
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (id, username, password) VALUES (%s, %s, %s)", (user_id, username, hashed_password))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return user_id
+
+def validate_user(username, password):
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, hashed_password))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+
+    return user
--- seg.py
+++ seg.py
@@ -10,17 +10,15 @@ net.to(device)
 net.load_state_dict(torch.load('best_model.pth', map_location=device))
 net.eval()
 
-
 # 图像分割函数
 def segment_image(image):
-    origin_shape = image.shape[:2]  # 原始图像形状
+    origin_shape = image.shape[:2]
     image = cv2.resize(image, (256, 256))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = image.transpose((2, 0, 1))
     image = image / 255.0
     image_tensor = torch.from_numpy(image).unsqueeze(0).to(device=device, dtype=torch.float32)
 
-    # 模型推理
     with torch.no_grad():
         pred = net(image_tensor)
         pred = torch.sigmoid(pred)
@@ -29,6 +27,5 @@ def segment_image(image):
         pred[pred < 0.5] = 0
         pred = pred.astype(np.uint8)
 
-    # 恢复到原始分辨率
     pred = cv2.resize(pred, (origin_shape[1], origin_shape[0]), interpolation=cv2.INTER_NEAREST)
     return pred
